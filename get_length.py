83cb828982352f98096b3a2b789d295d42a0de11
Jasurbek-code <samyboyborada@gmail.com>
2021-12-25 20:00:34 +0500

    raqam uzunligi

--- get_length.py
+++ get_length.py
@@ -1,8 +1,10 @@
 #get number of digits in an int?
+import math
 def get_length(num):
+    x1, x2, x3, x4 = 0, 0, 0, 0
     """
     Get length of integer
-
+    
     Args:
         num (int): integer to get length of
 
@@ -10,6 +12,13 @@ def get_length(num):
         int: length of integer
     """
     # return number of digits in integer
-    return 
+    x1 = num % 10
+    x2 = num // 10 % 10
+    x3 = num // 100 % 10
+    len = x1/x1 + x2/x2 + x3/x3
+    return num, "uzunligi=", int(len)
 
- 
+print(get_length(456))
+
+
+
