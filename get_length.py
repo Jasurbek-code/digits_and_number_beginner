5dcf344bb551bcc2930a508e313f7e060d6b7e14
Jasurbek-code <samyboyborada@gmail.com>
2021-12-25 21:45:36 +0500

    raqam uzunligi

--- get_length.py
+++ get_length.py
@@ -17,8 +17,8 @@ def get_length(num):
     x4 = num // 1000 % 10
     x5 = num // 10000 % 10
 
-    uz = x1-(x1-1)+x2-(x2-1)+x3-(x3-1)+x4-(x4-1)+x5-(x5-1)
-    return  uz
+    uzunlik = x1**0 + x2**0 + x3**0 + x4**0 + x5**0
+    return  uzunlik
 
 print(get_length(12346))
 
