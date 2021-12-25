7015248c59eb41da1bee0684729a484a70c4d779
Jasurbek-code <samyboyborada@gmail.com>
2021-12-25 20:23:19 +0500

    raqamlar yig'indisi

--- get_sum_digits.py
+++ get_sum_digits.py
@@ -10,4 +10,20 @@ def get_sum_digits(num):
         int: sum of digits in integer
     """
     # return sum of digits in integer
-    return  
+    x1,x2,x3,x4 = 0,0,0,0
+
+    x1 = num % 10
+    num //= 10
+
+    x2 = num % 10
+    num //= 10
+
+    x3 = num % 10
+    num //= 10
+
+    x4 = num % 10
+    num //= 10
+
+    Sum = x1+x2+x3+x4
+    return  Sum
+print(get_sum_digits(1234))
