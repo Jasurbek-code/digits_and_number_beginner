600397b57e020d47b825949dff6f619650607a5d
Jasurbek-code <samyboyborada@gmail.com>
2021-12-25 21:41:30 +0500

    raqam uzunligi

--- get_length.py
+++ get_length.py
@@ -10,15 +10,17 @@ def get_length(num):
         int: length of integer
     """
     # return number of digits in integer
+
     x1 = num % 10
     x2 = num // 10 % 10
     x3 = num // 100 % 10
     x4 = num // 1000 % 10
-    lens = x1/x1 + x2/x2 + x3/x3 + x4/x4
+    x5 = num // 10000 % 10
 
-    return int(lens)
+    uz = x1-(x1-1)+x2-(x2-1)+x3-(x3-1)+x4-(x4-1)+x5-(x5-1)
+    return  uz
 
-print(get_length(1234))
+print(get_length(12346))
 
 
 
